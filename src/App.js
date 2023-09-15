8563e9d991cdd459748b8e9b03e14729163bbc4c
dhrvshrm <shrmadhruv11@gmail.com>
2023-09-16 02:58:22 +0530

    add table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import EnhancedTable from "./components/EnhancedTable";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />{" "}
+      <Navbar /> <EnhancedTable />{" "}
     </div>
   );
 }
